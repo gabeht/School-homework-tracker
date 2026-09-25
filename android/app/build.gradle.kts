9a158e5e36727ffcef6ff18a42fe8e368f66241a
Gabe Holte <holte243@umn.edu>
2026-09-25 12:53:26 -0500

    build: fix duplicate goole-service plugins & package typos

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,10 +5,9 @@ plugins {
     // END: FlutterFire Configuration
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google.services")
 }
 dependencies{
-    implementation(platform("com.google.firebase: firebase-bom: 33.13.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
 }
 
 
