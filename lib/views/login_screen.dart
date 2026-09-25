c3496717404869232e9382d2ffae6c42cca66bfd
Gabe Holte <holte243@umn.edu>
2026-09-25 10:42:36 -0500

    feat: added login_screen dependency: signup_screen.dart WIP

---
+++ lib/views/login_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import '../presenters/auth_presenter.dart';
+import 'main_navigation.dart';
+import 'signup_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen ({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  //variable naming change should reflect type of Presenter eg. _authPresenter
+  final _presenter = AuthPresenter();
+
+  String? _errorMessage;
+
+  void _handleLogin() async {
+    final error = await _presenter.login(
+      _emailController.text.trim(),
+      _passwordController.text.trim(),
+    );
+
+    if (error != null){
+      setState(() => _errorMessage = error);
+    } else {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const MainNavigationScreen()),
+      );
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(
+        title: const Text('Login'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            if (_errorMessage != null)
+              Text(_errorMessage!, style: const TextStyle(color: Colors.blue)),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Password'),
+            ),
+            const SizedBox(height:20),
+            ElevatedButton(onPressed: _handleLogin, child: const Text('Login')),
+            TextButton(
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const SignupScreen()),
+                );
+              },
+              child: const Text('Don\'t have an account? Sign up here!'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
